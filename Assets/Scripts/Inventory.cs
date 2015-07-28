fc11e8d04e3d4957cd352514aab7ea00235d8a87
Alicia Stott <as8885@nyu.edu>
2015-07-28 10:32:29 -0400

    room 1 almost feature complete. room 2 almost feature complete. room two needs further development

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+	public static int[] inventoryArray = {0, 0, 0};
+	public Text inventoryText;
+	
+	void Update ()
+	{
+		inventoryText.text = "Keys " + "[" + inventoryArray [0] + "]" + "\n" + "Phone " + "[" + inventoryArray [1] + "]" + "\n" + "Wallet " + "[" + inventoryArray [2] + "]";
+		
+//		inventoryArray [0]++;      //Comment these 2 lines out when used in 
+//		inventoryArray [1] += 2;   //conjunction with the pick-up script. :)
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+	
+	public float speed = 5f;
+	public float turnSpeed = 90f;
+	public ForceMode forceMode;
+	
+	
+	// FixedUpdate is called once per physics frame
+	void FixedUpdate ()
+	{
+		// "GetAxis" returns a float from -1 to 1
+		// from a "virtual joystick"...
+		float x = Input.GetAxis ("Horizontal"); // imagine [A] = -1, [D] = +1
+		float y = Input.GetAxis ("Vertical"); // imagine [W] = +1, [S] = -1
+		
+		// transform.Translate (0f, 0f, y * speed * Time.deltaTime );
+		Rigidbody rbody = GetComponent<Rigidbody> ();
+		rbody.AddRelativeForce (0f, 0f, y * speed * Time.deltaTime, forceMode);
+		// rbody.AddForce ( Vector3.forward );
+		
+		transform.Translate (0f, 0f, y * speed * Time.deltaTime);
+		transform.Rotate (0f, x * turnSpeed * Time.deltaTime, 0f);
+		
+//		// scoring mechanic
+//		if (Input.GetKeyDown( KeyCode.Space) ) {
+//			GameLogic.instance.currentScore += 1;
+//		}
+	}
+	
+}
